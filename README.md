31c61bd1f02a902ab6288e4d7f9b000f0c1af41c
spraksqchen <sparksqchen@gmail.com>
2023-09-30 11:46:33 +0800

    feat: init infra modules (gateway oauth)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # AchoBeta-base
 achobeta team base service
+
+
+
+# before develop
+
+1. execute command in the root directory
+```shell
+git config core.hooksPath .githooks 
+chomd -R -x .githooks 
+```
---
+++ achobeta-infra-oauth/src/test/java/com/achobeta/www/oauth/AchobetaInfraOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achobeta.www.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchobetaInfraOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
