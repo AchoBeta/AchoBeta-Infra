489e0f98b1811b75b8b9654e7e9c424136943008
Jett <51713304+jettcc@users.noreply.github.com>
2023-09-30 15:42:03 +0800

    feat: init infra modules (gateway oauth) (#1)

---
+++ achobeta-infra-gateway/src/main/java/com/achobeta/www/gateway/AchobetaInfraGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.achobeta.www.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchobetaInfraGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchobetaInfraGatewayApplication.class, args);
+    }
+
+}
