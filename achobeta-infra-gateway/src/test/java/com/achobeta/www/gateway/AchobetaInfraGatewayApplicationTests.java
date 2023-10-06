27b07db495501ebcf3c91a7d8abf1d83dcbe76cf
BanTanger <1290288968@qq.com>
2023-10-06 17:13:31 +0800

    fix: fix elk docker deploy error es need chmod 777 so that open file and write logs

--- achobeta-infra-gateway/src/test/java/com/achobeta/www/gateway/AchobetaInfraGatewayApplicationTests.java
+++ achobeta-infra-gateway/src/test/java/com/achobeta/www/gateway/AchobetaInfraGatewayApplicationTests.java
@@ -1,13 +1,20 @@
 package com.achobeta.www.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class AchobetaInfraGatewayApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("这是一个日志埋点");
+        log.warn("这是一个警告");
+        log.error("这是一个错误");
+        log.debug("这是一个debug");
+        log.trace("这是一个链式追踪");
     }
 
 }
