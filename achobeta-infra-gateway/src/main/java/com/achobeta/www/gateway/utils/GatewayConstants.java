a6c345ec592ad31ff4f959727caa39f3d909fa44
Jett <jetttcc@gmail.com>
2023-10-06 13:06:55 +0800

    feat: add whitelist filters and related resources

---
+++ achobeta-infra-gateway/src/main/java/com/achobeta/www/gateway/utils/GatewayConstants.java
@@ -0,0 +1,18 @@
+package com.achobeta.www.gateway.utils;
+/**
+ * <span>
+ *     Define and store gateway constants
+ * </span>
+ *
+ * @author jettcc in 2023/10/6
+ * @version 1.0
+ */
+
+public class GatewayConstants {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    public static final String REQUEST_TYPE_GET = "GET";
+
+    public static final String REQUEST_TYPE_POST = "POST";
+}
