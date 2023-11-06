7b4dd4a5606a7c3c88af07b10e1407f35cedaf19
Jett <jetttcc@gmail.com>
2023-11-06 23:39:59 +0800

    feat: configuration accessible path list

--- achobeta-infra-oauth/src/main/java/com/achobeta/www/oauth/config/auth/AuthorizationContextManager.java
+++ achobeta-infra-oauth/src/main/java/com/achobeta/www/oauth/config/auth/AuthorizationContextManager.java
@@ -4,16 +4,12 @@ import com.achobeta.www.common.util.GlobalServiceStatusCode;
 import com.achobeta.www.common.util.SystemJsonResponse;
 import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.ReactiveAuthorizationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.server.authorization.AuthorizationContext;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.nio.file.AccessDeniedException;
@@ -31,36 +27,25 @@ import java.util.List;
 @Component
 @Slf4j
 public class AuthorizationContextManager implements ReactiveAuthorizationManager<AuthorizationContext> {
-
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
-
     @Override
     public Mono<AuthorizationDecision> check(Mono<Authentication> authentication, AuthorizationContext authorizationContext) {
-
-        log.debug("{}", authentication.toString());
-        ServerWebExchange exchange = authorizationContext.getExchange();
-        ServerHttpRequest request = exchange.getRequest();
-        String path = request.getURI().getPath();
-        log.debug("{}", path);
-
-        // 配置可访问路径列表, 现在没有需求, 全放开
-        List<String> needAuthorityList = new ArrayList<>(){{add("/**");}};
-
+        /*
+            配置可访问路径列表, 现在没有需求, 全放开, 这里只是留个模板以后好改
+            List<String> needAuthorityList = new ArrayList<>(){{add("/**");}};
+            AuthorizationDecision authorizationDecision = new AuthorizationDecision(true);
+         */
         return authentication
                 .filter(Authentication::isAuthenticated)
-                .flatMapIterable(auth -> {
-                    log.debug("{}", auth.getAuthorities().toString());
-                    return auth.getAuthorities();
-                })
+                .flatMapIterable(Authentication::getAuthorities)
                 .map(GrantedAuthority::getAuthority)
-                .any(needAuthorityList::contains)
+                .any(_ -> true)
                 .map(AuthorizationDecision::new)
-                .defaultIfEmpty(new AuthorizationDecision(true));
+                .defaultIfEmpty(new AuthorizationDecision(false));
     }
 
     @Override
     public Mono<Void> verify(Mono<Authentication> authentication, AuthorizationContext object) {
+        log.debug("verify: {}", authentication.toString());
         return check(authentication, object)
                 .filter(AuthorizationDecision::isGranted)
                 .switchIfEmpty(Mono.defer(() -> {
