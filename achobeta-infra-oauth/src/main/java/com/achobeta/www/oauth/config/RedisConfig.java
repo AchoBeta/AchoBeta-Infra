042a0147003647a99c9cd9a592ef45b904da81d6
Jett <jett@moego.pet>
2023-12-14 13:00:48 +0800

    feat: configure Redis data source

--- achobeta-infra-oauth/src/main/java/com/achobeta/www/oauth/config/RedisConfig.java
+++ achobeta-infra-oauth/src/main/java/com/achobeta/www/oauth/config/RedisConfig.java
@@ -2,12 +2,18 @@ package com.achobeta.www.oauth.config;
 
 import com.alibaba.fastjson2.support.spring6.data.redis.FastJsonRedisSerializer;
 import com.alibaba.fastjson2.support.spring6.data.redis.GenericFastJsonRedisSerializer;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 /**
  * <span>
@@ -22,6 +28,33 @@ public class RedisConfig {
     private static final String[] ACCEPT_TYPES_NAMES = {
             "org.springframework.security.core.context.SecurityContextImpl"
     };
+
+    @Value("${spring.data.redis.connect.timeout:2000}")
+    private Integer redisConnectTimeout = 2000;
+
+    @Value("${spring.data.redis.read.timeout:2000}")
+    private Integer redisReadTimeout = 2000;
+
+    @Bean
+    public LettuceClientConfiguration clientConfiguration() {
+
+        SocketOptions socketOptions = SocketOptions.builder()
+                .connectTimeout(Duration.ofMillis(redisConnectTimeout)).build();
+
+        ClientOptions clientOptions = ClientOptions.builder()
+                .autoReconnect(true)
+                .pingBeforeActivateConnection(true)
+
+                .disconnectedBehavior(ClientOptions.DisconnectedBehavior.ACCEPT_COMMANDS)
+                .socketOptions(socketOptions)
+                .build();
+
+        return LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofMillis(redisReadTimeout))
+                .clientOptions(clientOptions)
+                .build();
+    }
+
 
     @Bean
     public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
