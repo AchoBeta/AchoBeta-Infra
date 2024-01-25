47447dcaa8bd94aef953433cba418e1db850621c
Jett <jett@moego.pet>
2024-01-25 15:57:14 +0800

    feat(infra): add infra api service

---
+++ achobeta-infra-service/src/main/java/com/achobeta/www/service/AchobetaInfraServiceApplication.java
@@ -0,0 +1,13 @@
+package com.achobeta.www.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchobetaInfraServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AchobetaInfraServiceApplication.class, args);
+    }
+
+}
